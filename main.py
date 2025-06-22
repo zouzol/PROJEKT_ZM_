e7d4067988f2e32be176c98b54dbf8f4ab16d09d
Zuzia <zuzanna.majewskaa@wp.pl>
2025-06-22 17:30:22 +0200

    współrzędne

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ from tkinter import ttk, messagebox
 import tkintermapview
 import requests
 from bs4 import BeautifulSoup
+
+
+def get_coordinates(location):
+    try:
+        url = f'https://pl.wikipedia.org/wiki/{location}'
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, 'html.parser')
+        longitude = float(soup.select('.longitude')[1].text.replace(',', '.'))
+        latitude = float(soup.select('.latitude')[1].text.replace(',', '.'))
+        return [latitude, longitude]
+    except Exception:
+        return [52.23, 21.0]  # fallback: Warszawa
